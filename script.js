1030cf9c4a54f6ddbc25997e6e0d6701462d3468
tamygutz <sarai2008ixt@live.com.mx>
2022-01-30 13:20:20 -0600

    Ejercicios de Introducción a Java Script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Tamara Gutiérrez 
+// Ejercicios Introducción a Java Script 
 const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
@@ -35,3 +37,54 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+
+console.log("Tamara Gutiérrez"); 
+console.log("ejercicio1");
+
+function EsPrimo(numero){
+  let primo =true; 
+  for(indice=numero-1; indice>1; indice --){
+    if(numero%indice==0){
+      console.log(`el numero: ${numero} es divisible entre ${indice} por tanto no es primo`);
+      primo= false; 
+      break;
+    }
+  }
+if(primo==true){
+  console.log(`el numero: ${numero} es primo`);
+}
+}
+ejercicio1.forEach((numero) => {
+  EsPrimo(numero);
+}); 
+
+// ejercicio 2 
+console.log("////////////////////////////////////////////");
+console.log("ejercicio2"); 
+console.log("////////////////////////////////////////////");
+function Admitidos(persona){
+  if(persona.edad>=18 && persona.esFamiliar){
+    console.log(`${persona.nombre} es admitido en la fiesta`); 
+  }
+}
+
+ejercicio2.forEach((persona) => {
+  Admitidos(persona);
+}); 
+
+//ejercicio 3
+console.log("///////////////////////////////////////////");
+console.log("ejercicio 3"); 
+console.log("///////////////////////////////////////////"); 
+
+const sucesionfibonacci = (limite) => {
+  const fibonacci = [1,2];
+  for (let i=2 ; i<limite; i++){
+    fibonacci[i] = fibonacci [i-1] + fibonacci[i-2];
+  }
+  return fibonacci; 
+}
+
+let serieFibonacci = sucesionfibonacci(50);
+console.log(serieFibonacci);
